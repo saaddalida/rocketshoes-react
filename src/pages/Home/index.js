9c2750c58eb12dc110b9a7dea5075a746e6e6875
saaddalida <dalidassaad@gmail.com>
2019-07-15 15:40:56 -0300

    2 - Estilizaçao da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,107 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-lite-masculino/26/D12-3055-026/D12-3055-026_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Court Lite Masculino</strong>
+        <span>R$ 169,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-lite-masculino/26/D12-3055-026/D12-3055-026_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Court Lite Masculino</strong>
+        <span>R$ 169,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-lite-masculino/26/D12-3055-026/D12-3055-026_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Court Lite Masculino</strong>
+        <span>R$ 169,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-lite-masculino/26/D12-3055-026/D12-3055-026_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Court Lite Masculino</strong>
+        <span>R$ 169,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-lite-masculino/26/D12-3055-026/D12-3055-026_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Court Lite Masculino</strong>
+        <span>R$ 169,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-court-lite-masculino/26/D12-3055-026/D12-3055-026_detalhe2.jpg?resize=326:*"
+          alt="Tênis"
+        />
+        <strong>Tênis Nike Court Lite Masculino</strong>
+        <span>R$ 169,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
